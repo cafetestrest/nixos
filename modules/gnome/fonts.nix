d049a5a54fbdf9a5af82a308c286b491c1cceaf4
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2023-06-03 10:33:02 +0200

    keybinds for hyprland

--- modules/gnome/fonts.nix
+++ modules/gnome/fonts.nix
@@ -4,13 +4,13 @@
   fonts.fonts = with pkgs; [
     #$pkgs.nur.repos.sagikazarmark.sf-pro #SF Pro font (NUR): https://github.dev/sagikazarmark/nix-config
     ubuntu_font_family
-    font-awesome                    #for waybar icons
+    font-awesome                    # for waybar icons
     font-awesome_5
-    material-symbols       # waybar icon fonts
+    material-symbols                # waybar icon fonts
     noto-fonts-emoji
     nerdfonts
     jetbrains-mono
     roboto
-    montserrat                      #for rofi
+    montserrat                      # for rofi
   ];
 }
