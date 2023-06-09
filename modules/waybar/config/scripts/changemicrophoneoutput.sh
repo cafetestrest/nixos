efe13d026f48a8d4ae23d8a049a5249f34889c8d
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2023-06-09 20:48:09 +0200

    change mic output on script for waybar pulseaudio module

---
+++ modules/waybar/config/scripts/changemicrophoneoutput.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sinkNames=$(wpctl status | awk '/^ ├─ Sources:/,/^ ├─ Source endpoints:/ {if (NR>2) print}' | grep -E '*[0-9]+\.'| sed 's/\[.*//' | sed 's/.*[0-9]\+\.\s*//' | rofi -dmenu -i -p "Select Audio Output");
+
+if [ ! -z "$sinkNames" ]; then
+    sinkNumber=$(wpctl status | awk '/^ ├─ Sources:/,/^ ├─ Source endpoints:/ {if (NR>2) print}' | grep "${sinkNames}" | grep -E '*[0-9]+\.' | awk '{ for(i=1; i<=NF; i++) { if($i ~ /^[0-9]+\.$/) { print $i; break; } } }' | sed 's/\.$//');
+
+    if [ ! -z "$sinkNumber" ]; then
+        wpctl set-default $sinkNumber
+    fi
+fi
