8a65fdb91cdd6de2bb12539ea047edf118142ce3
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2023-06-09 21:53:48 +0200

    change audio word to microphone word

--- modules/waybar/config/scripts/changemicrophoneoutput.sh
+++ modules/waybar/config/scripts/changemicrophoneoutput.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sinkNames=$(wpctl status | awk '/^ ├─ Sources:/,/^ ├─ Source endpoints:/ {if (NR>2) print}' | grep -E '*[0-9]+\.'| sed 's/\[.*//' | sed 's/.*[0-9]\+\.\s*//' | rofi -dmenu -i -p "Select Audio Output");
+sinkNames=$(wpctl status | awk '/^ ├─ Sources:/,/^ ├─ Source endpoints:/ {if (NR>2) print}' | grep -E '*[0-9]+\.'| sed 's/\[.*//' | sed 's/.*[0-9]\+\.\s*//' | rofi -dmenu -i -p "Select Microphone Output");
 
 if [ ! -z "$sinkNames" ]; then
     sinkNumber=$(wpctl status | awk '/^ ├─ Sources:/,/^ ├─ Source endpoints:/ {if (NR>2) print}' | grep "${sinkNames}" | grep -E '*[0-9]+\.' | awk '{ for(i=1; i<=NF; i++) { if($i ~ /^[0-9]+\.$/) { print $i; break; } } }' | sed 's/\.$//');
