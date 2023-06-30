460508984e08ae6dda099cce1e181472d1d40bca
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2023-06-30 11:28:00 +0200

    updated home-manager to look at release-23.05 and run the flake update and system upgrade

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     };
 
     home-manager = {
-      url = github:nix-community/home-manager;
+      url = github:nix-community/home-manager/release-23.05;
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
