d5e5dd911c9f32ce5fd251021f2efc3dd0bf6291
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2023-07-08 21:08:06 +0200

    Adds logic that checks for playing media and pauses it on startup, usually on chromium

--- modules/hyprland/config/scripts/startup.sh
+++ modules/hyprland/config/scripts/startup.sh
@@ -14,4 +14,24 @@ function open_apps() {
     hyprctl dispatch swapnext
 }
 
+function is_playing_media()
+{
+    max_duration=20  # Maximum duration to check (in seconds)
+    interval=1     # Interval between checks (in seconds)
+    elapsed=0        # Elapsed time counter
+
+    while [ $elapsed -lt $max_duration ]; do
+        status=$(playerctl status)
+
+        if [ "$status" = "Playing" ]; then
+            playerctl pause
+            break
+        fi
+
+        sleep 0.5
+        elapsed=$(bc <<< "$elapsed + $interval")
+    done
+}
+
 open_apps
+is_playing_media
