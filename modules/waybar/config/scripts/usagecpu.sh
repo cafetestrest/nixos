8cd7ee504c5d977909e0aa2f0cf81fa207b5df88
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2023-07-29 20:10:27 +0200

    added cpuusage script

---
+++ modules/waybar/config/scripts/usagecpu.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+usage=$(top -bn1 | grep "Cpu(s)" | \
+           sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
+           awk '{print 100 - $1"%"}')
+
+firstArg="$(echo "$1")"
+
+if [[ $firstArg == "ags" ]]; then
+    ags run-js "ags.Service.Usage.setCpuUsage('${usage}')"
+else
+    echo ${usage}
+fi
