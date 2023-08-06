03b3df7d3088b1b594adf832ee3c698757b58751
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2023-08-06 19:34:40 +0200

    added nix-community cachix

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -125,6 +125,15 @@ in
     extraOptions = "${nixExtraOptions}";
   };
 
+  nix.settings = {
+    substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   nix.settings.auto-optimise-store = true;
 
   #support ntfs hard drive 29-mar-2023
