a39e2d8c17c913cf446bfeed8e0eb75ad88ee1af
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2023-08-12 17:50:42 +0200

    updated gtklock

--- modules/gtklock/default.nix
+++ modules/gtklock/default.nix
@@ -44,7 +44,7 @@ in
 [main]
 modules=/run/current-system/sw/lib/gtklock/powerbar-module.so;/run/current-system/sw/lib/gtklock/userinfo-module.so
 time-format=%R
-layout=/home/bajic/.config/gtklock/gtklock.ui
+layout=${homeDirectory}/.config/gtklock/gtklock.ui
 
 [userinfo]
 under-clock=true
