44bfbda9456354ddba4893a3cf63ffdb7d845a21
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2023-08-26 14:54:04 +0200

    closed the brackets properly

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -12,30 +12,30 @@ in
         source = ./weather.sh;
         executable = true;
       };
-    };
 
-    #swayidle used to fine crontrol swayidle -> toggle/startup/icon script
-    ".config/scripts/swayidle.sh" = {
-      source = ./swayidle.sh;
-      executable = true;
-    };
+      #swayidle used to fine crontrol swayidle -> toggle/startup/icon script
+      ".config/scripts/swayidle.sh" = {
+        source = ./swayidle.sh;
+        executable = true;
+      };
 
-    #night light config script
-    ".config/scripts/nightlight.sh" = {
-      source = ./nightlight.sh;
-      executable = true;
-    };
+      #night light config script
+      ".config/scripts/nightlight.sh" = {
+        source = ./nightlight.sh;
+        executable = true;
+      };
 
-    #script for control of note (waybar and ags) - note.sh
-    ".config/scripts/note.sh" = {
-      source = ./note.sh;
-      executable = true;
-    };
+      #script for control of note (waybar and ags) - note.sh
+      ".config/scripts/note.sh" = {
+        source = ./note.sh;
+        executable = true;
+      };
 
-    #used for screenshot, left and right click (waybar and ags) - screenshot.sh
-    ".config/scripts/screenshot.sh" = {
-      source = ./screenshot.sh;
-      executable = true;
+      #used for screenshot, left and right click (waybar and ags) - screenshot.sh
+      ".config/scripts/screenshot.sh" = {
+        source = ./screenshot.sh;
+        executable = true;
+      };
     };
   };
 }
