df63b322535b59c410923d01bd03b8fbeb185dc5
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2023-09-15 22:08:49 +0200

    updated gtk settings

--- modules/gnome/dconf-settings.nix
+++ modules/gnome/dconf-settings.nix
@@ -13,18 +13,18 @@
 
     "org/gnome/desktop/interface" = {
       #theme
-      gtk-theme = "Orchis-Dark";
+      # gtk-theme = "Orchis-Dark";
 
       #fonts
-      font-name = "Cantarell 11";
+      # font-name = "Cantarell 11";
       document-font-name = "Cantarell 11";
       monospace-font-name = "Source Code Pro 10";
 
       #cursor
-      cursor-theme = "macOS-Monterey";
+      # cursor-theme = "macOS-Monterey";
 
       #icon
-      icon-theme = "Adwaita";
+      # icon-theme = "Adwaita";
 
       #clock in top bar
       clock-show-seconds = true;
--- modules/home.nix
+++ modules/home.nix
@@ -46,22 +46,36 @@ in
     # btop
   ];
 
-  # gtk = {
-  #   enable = true;
-  #   font = {
-  #     name = "Cantarell 11";
-  #   };
-  #   theme = {
-  #     name = "Adwaita-Dark";
-  #     # package = pkgs.themename;
-  #   };
-  #   cursorTheme = {
-  #     name = "macOS-Monterey";
-  #     package = pkgs.apple-cursor;
-  #   };
-  #   # iconTheme = {
-  #     # name = "Adwaita";
-  #     # package = pkgs-iconname;
-  #   # };
-  # };
+  gtk = {
+    enable = true;
+    font.name = "Cantarell 11";
+    theme = {
+      name = "Orchis-Dark";
+      # package = pkgs.juno-theme;
+    };
+    iconTheme = {
+      name = "Adwaita";
+      # package = pkgs.papirus-icon-theme;
+    };
+
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+  };
+
+  home.pointerCursor = {
+    size = 24;
+    gtk.enable = true;
+    # x11.enable = true;
+    package = pkgs.apple-cursor;
+    name = "macOS-Monterey";
+  };
 }
