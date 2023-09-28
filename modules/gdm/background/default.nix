bb4e65738e9534438c383c78a2817f1427dfe73b
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2023-09-28 19:16:21 +0200

    gdm background attempts

---
+++ modules/gdm/background/default.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+
+let
+  inherit (import ../../../variables.nix)
+    user;
+in
+{
+  # GNOME login screen patch
+  nixpkgs = {
+    overlays = [
+      (self: super: {
+        gnome = super.gnome.overrideScope' (selfg: superg: {
+          gnome-shell = superg.gnome-shell.overrideAttrs (old: {
+            patches = (old.patches or []) ++ [
+              (pkgs.substituteAll {
+                src = ./gnome-shell.patch;
+              })
+            ];
+          });
+        });
+      })
+    ];
+  };
+
+  services.xserver.desktopManager.gnome.extraGSettingsOverrides = ''
+  [com.ubuntu.login-screen]
+  background-repeat='no-repeat'
+  background-size='cover'
+  background-color='#777777'
+  background-picture-uri='file:///etc/nixos/background.jpg'
+  '';
+}
