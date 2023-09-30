db4d639f79fa7ee2a7756e3aa1f149a75129a0e6
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2023-09-30 19:05:28 +0200

    added wallpaper to gdm

--- modules/gdm/background/default.nix
+++ modules/gdm/background/default.nix
@@ -2,32 +2,35 @@
 
 let
   inherit (import ../../../variables.nix)
+    homeDirectory
     user;
 in
 {
-  # GNOME login screen patch
   nixpkgs = {
     overlays = [
       (self: super: {
         gnome = super.gnome.overrideScope' (selfg: superg: {
           gnome-shell = superg.gnome-shell.overrideAttrs (old: {
             patches = (old.patches or []) ++ [
-              (pkgs.substituteAll {
-                backgroundColour = "#d94360";
-                src = ./gnome-shell.patch;
-              })
+              (let
+                # bg = pkgs.fetchurl {
+                #   url = "https://orig00.deviantart.net/0054/f/2015/129/b/9/reflection_by_yuumei-d8sqdu2.jpg";
+                #   sha256 = "0f0vlmdj5wcsn20qg79ir5cmpmz5pysypw6a711dbaz2r9x1c79l";
+                # };
+              in pkgs.writeText "bg.patch" ''
+                --- a/data/theme/gnome-shell-sass/widgets/_login-lock.scss
+                +++ b/data/theme/gnome-shell-sass/widgets/_login-lock.scss
+                @@ -15,4 +15,5 @@ $_gdm_dialog_width: 23em;
+                 /* Login Dialog */
+                 .login-dialog {
+                   background-color: $_gdm_bg;
+                +  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('file:///etc/nixos/lockscreen.jpg');
+                 }
+              '')
             ];
           });
         });
       })
     ];
   };
-
-  services.xserver.desktopManager.gnome.extraGSettingsOverrides = ''
-  [com.ubuntu.login-screen]
-  background-repeat='no-repeat'
-  background-size='cover'
-  background-color='#777777'
-  background-picture-uri='file:///etc/nixos/background.jpg'
-  '';
 }
