970e3e26f30b7293ef858decfe6183e9f3375d1c
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2023-12-21 19:05:44 +0100

    warden bootstrap m2 command

--- modules/docker/warden.nix
+++ modules/docker/warden.nix
@@ -7,14 +7,15 @@
       installPhase = ''
         mkdir -p $out/bin
         cp $src/bin/warden $out/bin/warden
-        cp -r $src/config $out/
-        cp -r $src/docker $out/
-        cp -r $src/environments $out/
-        cp -r $src/utils $out/
-        cp $src/version $out/
-
-        mkdir -p $out/commands
+        cp -r config $out/
+        cp -r docker $out/
+        cp -r utils $out/
         cp -r commands/ $out/
+        cp -r environments $out/
+        cp version $out/
+
+        mkdir -p $out/environments/magento2/.warden
+        cp -r ${./warden/.warden}/* $out/environments/magento2/.warden
       '';
 
       src = fetchFromGitHub {
@@ -24,7 +25,11 @@
         sha256 = "sha256-GK27z72n3jTU5BQN79MITi5qtICDWfO1tYg5gVcOz+4=";
       };
 
-      patches = [ ./patch/warden-svc-up-detach.patch ];
+      patches = [
+        ./patch/warden-svc-up-detach.patch
+        ./patch/warden-env-init.patch
+        ./patch/env-init-file.patch
+      ];
     };
   in
   [
