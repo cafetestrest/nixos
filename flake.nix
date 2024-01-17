9ee51fe6cea418ff0bee3d63f00f31703432c904
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2024-01-17 17:27:45 +0100

    flake lock

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
 
     hyprland = {
       url = github:hyprwm/Hyprland;
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
     hyprland-plugins.url = "github:hyprwm/hyprland-plugins";
