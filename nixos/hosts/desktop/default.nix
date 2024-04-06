0b26677d0e78d64eb5ba9c9e2716d7a119e7f180
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2024-04-06 21:59:51 +0200

    boot flags

--- nixos/hosts/desktop/default.nix
+++ nixos/hosts/desktop/default.nix
@@ -24,8 +24,41 @@ in
   ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
   #boot.loader.efi.efiSysMountPoint = "${efiSysMountPoint}";
-  boot.loader.systemd-boot.configurationLimit = configurationLimit;
+  # boot.loader.systemd-boot.configurationLimit = configurationLimit;
+
+  # Boot
+  boot =
+  {
+    # Stay up-to-date on the kernel.
+    bootspec.enable = true;
+
+    # Boot Loader
+    loader =
+    {
+      # timeout = 0;
+      systemd-boot.editor = false;
+      systemd-boot.enable = true;
+      systemd-boot.configurationLimit = configurationLimit;
+      efi.canTouchEfiVariables = true;
+    };
+
+    # Silent Boot
+    kernelParams = [
+      "quiet"
+      "splash"
+      "vga=current"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+      "boot.shell_on_fail"
+    ];
+
+    # Plymouth
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    # plymouth.enable = true;
+  };
 }
