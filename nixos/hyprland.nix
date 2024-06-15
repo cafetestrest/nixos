f03693d297941654b94e609af23b698b7854a063
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2024-06-15 09:53:45 +0200

    hyprland cachix

--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -19,10 +19,10 @@
   #   pam.services.ags = {};
   # };
 
-  # nix.settings = {
-  #   substituters = ["https://hyprland.cachix.org"];
-  #   trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-  # };
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
 
   # environment.sessionVariables.NIXOS_OZONE_WL = "1"; # Hint electron apps to use wayland
 
