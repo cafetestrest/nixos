c71343d3e3f8b2eec1d2111fce285a6bd89506d8
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2024-06-28 22:15:17 +0200

    added vscode extensions

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -10,45 +10,18 @@
       # enableUpdateCheck = false;
       # mutableExtensionsDir = false;
 
-    #   extensions = with pkgs.vscode-extensions; [
-    #     redhat.java
-    #     vscjava.vscode-java-debug
-    #     matklad.rust-analyzer
-    #     jnoortheen.nix-ide
-
-    #     catppuccin.catppuccin-vsc
-    #     catppuccin.catppuccin-vsc-icons
-    #     roman.ayu-next
-    #     ms-vscode.cpptools
-    #     #ms-python.python
-    #     #ms-python.vscode-pylance
-    #     #ms-azuretools.vscode-docker
-    #   ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-    #     {
-    #       name = "mayukaithemevsc";
-    #       publisher = "GulajavaMinistudio";
-    #       version = "3.2.3";
-    #       sha256 = "a0f3c30a3d16e06c31766fbe2c746d80683b6211638b00b0753983a84fbb9dad";
-    #     }
-    #     {
-    #       name = "vscode-scheme";
-    #       publisher = "sjhuangx";
-    #       version = "0.4.0";
-    #       sha256 = "sha256-BN+C64YQ2hUw5QMiKvC7PHz3II5lEVVy0Shtt6t3ch8=";
-    #     }
-    #     {
-    #       name = "vscode-power-mode";
-    #       publisher = "hoovercj";
-    #       version = "3.0.2";
-    #       sha256 = "sha256-ZE+Dlq0mwyzr4nWL9v+JG00Gllj2dYwL2r9jUPQ8umQ=";
-    #     }
-    #     {
-    #       name = "subway-surfers";
-    #       publisher = "jirkavrba";
-    #       version = "2.0.0";
-    #       sha256 = "sha256-gZ4dlt6T3pZgxz/voeENVVMJEJ/k99mszDXwEQzl04I=";
-    #     }
-    #   ];
+      extensions = with pkgs.vscode-extensions; [
+        jnoortheen.nix-ide
+        christian-kohler.path-intellisense
+      ];
+      # ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      #   {
+      #     name = "mayukaithemevsc";
+      #     publisher = "GulajavaMinistudio";
+      #     version = "3.2.3";
+      #     sha256 = "a0f3c30a3d16e06c31766fbe2c746d80683b6211638b00b0753983a84fbb9dad";
+      #   }
+      # ];
 
     #   userSettings = {
     #     "workbench.iconTheme" = "catppuccin-macchiato";
