a8c3d56be01e752c80767e8ea003433fb0d7bdcb
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2024-06-30 15:43:31 +0200

    lock update, warden changes

--- variables.nix
+++ variables.nix
@@ -14,7 +14,7 @@ in
   efiSysMountPoint = "/boot/efi";
   grubHardDriveForVM = "/dev/vda";
   configurationLimit = 20;
-  wardenSha256Hash = "sha256-A5jBH5VY2KhYVyJ5gCAXZpMX0au/W/cRK8AhFXyyu/M="; #nix-shell -p nix-prefetch-git jq --run "nix hash to-sri sha256:\$(nix-prefetch-git --url https://github.com/wardenenv/warden --quiet --rev refs/heads/main | jq -r '.sha256')"
+  wardenSha256Hash = "sha256-3C7xoJdtAeiuQdNqT0WnSafSILDX97Ro5Pt/UIuVg9k="; #nix-shell -p nix-prefetch-git jq --run "nix hash to-sri sha256:\$(nix-prefetch-git --url https://github.com/wardenenv/warden --quiet --rev refs/heads/main | jq -r '.sha256')"
   cursorSize = 24;
   cursorTheme = "macOS-Monterey";
   gtkTheme = "Orchis-Dark";
