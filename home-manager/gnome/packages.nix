935db6cf96e52908f7d51a9ebcc2d3f711044b7e
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2024-06-30 16:08:46 +0200

    added gtk settings for gtk theme... maybe refactor later if needed

--- home-manager/gnome/packages.nix
+++ home-manager/gnome/packages.nix
@@ -1,8 +1,19 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   inherit (import ../../variables.nix)
+    cursorSize
+    cursorTheme
+    gtkIconTheme
+    gtkFontName
     gtkTheme;
+
+    orchis = (pkgs.orchis-theme.override {
+      border-radius = 3;
+      tweaks = [ "compact" "macos" "submenu" ];
+    });
+
+  cursor-package = pkgs.apple-cursor;
 in
 {
   #   # logic that allows to switch to unstable (use fcitx5 instead of deprecated fcitx-engines)
@@ -20,12 +31,41 @@ in
     gnome.eog
     # xpad                      #sticky notes
     shotwell                #photo editor for gnome
-
-    (orchis-theme.override {
-        border-radius = 3;
-        tweaks = [ "compact" "macos" "submenu" ];
-    })
+    orchis
   ];
+
+  gtk = {
+      enable = true;
+
+      font.name = "${gtkFontName}";
+
+      theme = {
+          name = "${gtkTheme}";
+          package = orchis;
+      };
+
+      iconTheme = {
+          name = "${gtkIconTheme}";
+      };
+
+      cursorTheme = {
+          name = "${cursorTheme}";
+      };
+
+      gtk3.extraConfig = {
+          gtk-application-prefer-dark-theme=1;
+      };
+
+      gtk4.extraConfig = {
+          gtk-application-prefer-dark-theme=1;
+      };
+  };
+
+  xdg.configFile = {
+    "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+    "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+    "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+  };
 
   home.sessionVariables.GTK_THEME = "${gtkTheme}";
 }
