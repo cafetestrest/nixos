40c6beb92772db81e8ce72057c528e89c38e8db3
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2024-07-25 21:48:29 +0200

    added kitty config

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -4,4 +4,14 @@
   programs.kitty = {
     enable = true;
   };
+
+  xdg.configFile."kitty/kitty.conf" = {
+    source = ../config/terminal/kitty/kitty.conf;
+  };
+
+  # kitty folder
+  # xdg.configFile."kitty" = {
+  #   source = ../config/terminal/kitty;
+  #   recursive = true;
+  # };
 }
