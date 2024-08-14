c9df1b1ecde92bc3eab01e29b8af2c8454e5504a
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2024-08-14 16:27:32 +0200

    removed ddcutil requirement, moved back to headset sh file and included runtimeInputs on scripts

--- nixos/hosts/packages.nix
+++ nixos/hosts/packages.nix
@@ -6,7 +6,6 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     apple-cursor
-    ddcutil
     # teamviewer
   ];
 
