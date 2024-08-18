7bf8e935580f208183b86f4136d45a99e6feb4c6
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2024-08-18 09:22:03 +0200

    setting ydotool back to one-line

--- nixos/ydotool.nix
+++ nixos/ydotool.nix
@@ -1,41 +1,11 @@
 { config, pkgs, lib, vars, ... }:
 
 {
-  # programs.ydotool.enable = true;
-  users.groups.ydotool = { };
-
-  systemd.services.ydotoold = {
-    description = "ydotoold - backend for ydotool";
-    wantedBy = [ "multi-user.target" ];
-    partOf = [ "multi-user.target" ];
-    serviceConfig = {
-      Group = "ydotool";
-      RuntimeDirectory = "ydotoold";
-      RuntimeDirectoryMode = "0750";
-      ExecStart = "${lib.getExe' pkgs.ydotool "ydotoold"} --socket-path=/run/ydotoold/socket --socket-perm=0660";
-
-      # hardening
-
-      ## allow access to uinput
-      DeviceAllow = [ "/dev/uinput" ];
-      DevicePolicy = "closed";
-
-      ## allow creation of unix sockets
-      RestrictAddressFamilies = [ "AF_UNIX" ];
-
-    };
-  };
-
-  environment.variables = {
-    YDOTOOL_SOCKET = "/run/ydotoold/socket";
-  };
-  environment.systemPackages = with pkgs; [ ydotool ];
+  programs.ydotool.enable = true;
 
   users.users.${vars.user} = {
     extraGroups = [
-      "ydotool"
-      "input"
-      "uinput"
+      "ydotool" #TODO remove on 24.11
     ];
   };
 }
