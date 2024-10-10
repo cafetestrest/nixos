8d98fd9537a82736a1f8dc60276a2e8ac3a24a9c
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2024-10-10 23:17:09 +0200

    fixed extra portals enabled only when gnome is disabled

--- nixos/xdg.nix
+++ nixos/xdg.nix
@@ -5,6 +5,7 @@ with lib;
 let
   cfg = config.module.xdg;
   cfgHyprland = config.module.desktop-environment.hyprland;
+  cfgGnome = config.module.desktop-environment.gnome;
 in
 {
   options = {
@@ -14,7 +15,9 @@ in
   config = mkIf cfg.enable {
     xdg.portal = {
       enable = true;
-      extraPortals = lib.optionals cfgHyprland.enable [ pkgs.xdg-desktop-portal-gtk ];
+      extraPortals = lib.optionals cfgHyprland.enable (
+        if cfgGnome.enable then [] else [ pkgs.xdg-desktop-portal-gtk ]
+      );
     };
   };
 }
