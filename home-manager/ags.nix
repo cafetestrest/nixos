e6eca3542fb5d90a0bc91f49d8066fcc45aace54
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2024-12-27 18:41:16 +0100

    functions to enable old ags commands in bash/fish

--- home-manager/ags.nix
+++ home-manager/ags.nix
@@ -4,6 +4,9 @@ with lib;
 
 let
   cfg = config.module.bar.ags;
+  cfgBashrc = config.module.shell.bash.bashrc;
+  cfgBash = config.module.shell.bash;
+  cfgFish = config.module.shell.fish;
 in
 {
   options = {
@@ -44,6 +47,39 @@ in
         inputs.ags.packages.${pkgs.system}.bluetooth
         # inputs.ags.packages.${pkgs.system}.auth
       ];
+    };
+
+    programs.bash.bashrcExtra = lib.mkIf (cfgBash.enable && cfgBashrc.enable) ''
+      function ags () {
+        if [ "$#" -eq 0 ]; then
+          # No arguments: Run `ags run`
+          command ags run
+        elif [[ "$1" == "-q" || "$1" == "q" ]]; then
+          # If the first argument is `-q`, replace it with `quit`
+          shift  # Remove the first argument (`-q`)
+          command ags quit "$@"
+        else
+          # Otherwise, pass all arguments as-is
+          command ags "$@"
+        fi
+      }
+    '';
+
+    programs.fish.functions = lib.mkIf cfgFish.enable {
+      ags = {
+        body = ''
+          if test (count $argv) -eq 0
+            # No arguments: Run `ags run`
+            command ags run
+          else if test "$argv[1]" = "-q" -o "$argv[1]" = "q"
+            # If the first argument is `-q`, replace it with `quit`
+            command ags quit $argv[2..-1]
+          else
+            # Otherwise, pass all arguments as-is
+            command ags $argv
+          end
+        '';
+      };
     };
   };
 }
