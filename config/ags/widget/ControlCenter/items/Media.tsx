c0324c24a16cf38079af9ef87667b9202a0913ff
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2025-01-02 12:16:18 +0100

    ags updated player styling

--- config/ags/widget/ControlCenter/items/Media.tsx
+++ config/ags/widget/ControlCenter/items/Media.tsx
@@ -3,6 +3,7 @@ import { bind, Variable } from "astal";
 import AstalMpris from "gi://AstalMpris?version=0.1";
 import icons from "../../../lib/icons";
 import { lookUpIcon } from "../../../lib/utils";
+import GdkPixbuf from 'gi://GdkPixbuf';
 
 type PlayerProps = {
 	player: AstalMpris.Player;
@@ -36,7 +37,7 @@ const Player = ({ player }: PlayerProps) => {
 
 	const ControlButton = ({ icon, onClick, className }: { icon: string; onClick: () => void; className: string }) => (
 		<button hexpand={false} valign={Gtk.Align.CENTER} onClicked={onClick} className={className}>
-			<icon icon={icon} />
+			<icon icon={icon} className={`${className}-icon`}/>
 		</button>
 	);
 
@@ -53,6 +54,7 @@ const Player = ({ player }: PlayerProps) => {
 			}}
 		>
 			<icon
+				className={"player__playpause-icon"}
 				icon={bind(player, "playbackStatus").as((status) =>
 					status === AstalMpris.PlaybackStatus.PLAYING ? icons.media.playing : icons.media.stopped,
 				)}
@@ -79,12 +81,12 @@ const Player = ({ player }: PlayerProps) => {
 	);
 
 	const coverArt = bind(player, 'coverArt').as(
-        (c) => {
+		(c) => {
 			if (c)
 				return `background-image: url('${c}')`
 			return `background-image: none`
 		},
-    )
+	)
 
 	function lengthStr(length: number) {
 		const min = Math.floor(length / 60);
@@ -93,12 +95,118 @@ const Player = ({ player }: PlayerProps) => {
 		return `${min}:${sec0}${sec}`;
 	}
 
+	// function getAverageTextColorFromImage(coverArt: string) {
+	// 	const textColor = 'white';
+
+	// 	if (!coverArt) {
+	// 		return textColor;
+	// 	}
+
+	// 	// Load the image
+	// 	const pixbuf = GdkPixbuf.Pixbuf.new_from_file(coverArt);
+
+	// 	if (pixbuf) {
+	// 		const width = pixbuf.get_width();
+	// 		const height = pixbuf.get_height();
+	// 		const pixels = pixbuf.get_pixels();
+	// 		const rowstride = pixbuf.get_rowstride();
+	// 		const nChannels = pixbuf.get_n_channels();
+
+	// 		let totalR = 0, totalG = 0, totalB = 0;
+	// 		let pixelCount = 0;
+
+	// 		// Iterate over each pixel
+	// 		for (let y = 0; y < height; y++) {
+	// 			for (let x = 0; x < width; x++) {
+	// 				const offset = y * rowstride + x * nChannels;
+
+	// 				const r = pixels[offset];
+	// 				const g = pixels[offset + 1];
+	// 				const b = pixels[offset + 2];
+
+	// 				totalR += r;
+	// 				totalG += g;
+	// 				totalB += b;
+	// 				pixelCount++;
+	// 			}
+	// 		}
+
+	// 		// Calculate average color
+	// 		const avgR = totalR / pixelCount;
+	// 		const avgG = totalG / pixelCount;
+	// 		const avgB = totalB / pixelCount;
+
+	// 		// Invert the average color
+	// 		const invertedR = 255 - avgR;
+	// 		const invertedG = 255 - avgG;
+	// 		const invertedB = 255 - avgB;
+
+	// 		// Create CSS color from inverted RGB
+	// 		return `rgb(${invertedR}, ${invertedG}, ${invertedB})`;
+	// 	}
+
+	// 	return textColor;
+	// }
+
+	function getTextColorFromImage(coverArt: string) {
+		const textColor = 'white';
+
+		if (!coverArt) {
+			return textColor;
+		}
+
+		// Load the image
+		const pixbuf = GdkPixbuf.Pixbuf.new_from_file(coverArt);
+
+		if (pixbuf) {
+			const width = pixbuf.get_width();
+			const height = pixbuf.get_height();
+			const pixels = pixbuf.get_pixels();
+			const rowstride = pixbuf.get_rowstride();
+			const nChannels = pixbuf.get_n_channels();
+
+			let totalBrightness = 0;
+			let pixelCount = 0;
+
+			// Iterate over each pixel
+			for (let y = 0; y < height; y++) {
+				for (let x = 0; x < width; x++) {
+					const offset = y * rowstride + x * nChannels;
+
+					const r = pixels[offset];
+					const g = pixels[offset + 1];
+					const b = pixels[offset + 2];
+
+					// Calculate brightness
+					const brightness = (0.299 * r + 0.587 * g + 0.114 * b);
+					totalBrightness += brightness;
+					pixelCount++;
+				}
+			}
+
+			// Average brightness
+			const avgBrightness = totalBrightness / pixelCount;
+
+			// Determine text color
+			return avgBrightness > 128 ? 'black' : 'white';
+		}
+
+		return textColor;
+	}
+
 	return (
 		<box
 			name={player.busName}
 			vertical
 			className={`player player-${player.busName}`}
 			vexpand
+			css={bind(player, 'coverArt').as((c) => {
+				const textColor = getTextColorFromImage(c);
+
+				if (c)
+					return `background-image: radial-gradient(circle, rgba(0,0,0, 0.25) 10%, rgba(0,0,0, 0.25)), url("${c}"); color: ${textColor};`;
+				return `background-image: none`
+			})}
 			visible={bind(player, "playback_status").as((v) => {
 				if (v != 2) {
 					return true;
@@ -107,9 +215,9 @@ const Player = ({ player }: PlayerProps) => {
 			})}
 		>
 			<box className={"cover-box"} vertical={false}>
-				<box>
+				{/* <box>
 					<box className="cover-art" css={coverArt} />
-				</box>
+				</box> */}
 				<box vertical>
 					<box>
 						<box vertical halign={Gtk.Align.START} vexpand valign={Gtk.Align.CENTER} className="player__title-box"
@@ -211,12 +319,12 @@ const PlayerSwitcher = ({ mpris, selectedPlayer }: { mpris: AstalMpris.Mpris; se
 export default () => {
 	const mpris = AstalMpris.get_default();
 
-    return <box vertical className={"qs-media-indicator"}>
-        {bind(mpris, "players").as(arr => arr.map(player => {
+	return <box vertical className={"qs-media-indicator"}>
+		{bind(mpris, "players").as(arr => arr.map(player => {
 				return (<Player player={player} />)
 			}
 		))}
-    </box>
+	</box>
 
 	// const selectedPlayer = Variable<string>("");
 	// return <PlayerSwitcher mpris={mpris} selectedPlayer={selectedPlayer} />;
