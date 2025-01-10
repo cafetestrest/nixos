a04e04dab0097d2c9930be385c998e171842f1b1
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2025-01-10 16:02:20 +0100

    ags updated sys tray to support all clicks and update properly

--- config/ags/widget/Bar/items/Tray.tsx
+++ config/ags/widget/Bar/items/Tray.tsx
@@ -1,83 +1,86 @@
-import Tray from "gi://AstalTray";
-import { bind } from "astal";
-import { App, Gtk, Gdk } from "astal/gtk3";
-import BarItem from "../BarItem";
+import { Gdk, Widget, Gtk, App } from "astal/gtk3";
+import { bind, Variable, timeout, Gio, GLib, AstalIO } from "astal";
+import AstalTray from "gi://AstalTray";
 
-type BarTrayItemProps = {
-	item: Tray.TrayItem;
-};
+type TrayItem = ReturnType<ReturnType<typeof AstalTray.Tray.get_default>["get_item"]>;
 
-const BarTrayItem = ({ item }: BarTrayItemProps) => {
-	if (item.iconThemePath) App.add_icons(item.iconThemePath);
+function createMenu(menuModel: Gio.MenuModel, actionGroup: Gio.ActionGroup): Gtk.Menu {
+	const menu: Gtk.Menu = Gtk.Menu.new_from_model(menuModel);
+	menu.insert_action_group("dbusmenu", actionGroup);
+	return menu;
+}
 
-	// return (<menubutton
-	// 	tooltipMarkup={bind(item, "tooltipMarkup")}
-	// 	usePopover={false}
-	// 	actionGroup={bind(item, "action-group").as((ag) => [
-	// 	  "dbusmenu",
-	// 	  ag,
-	// 	])}
-	// 	menuModel={bind(item, "menu-model")}
-	// >
-	// 	<icon gicon={bind(item, "gicon")} />
-	// </menubutton>);
+const SysTrayItem = (item: TrayItem) => {
+	let menu: Gtk.Menu = createMenu(item.menu_model, item.action_group);
 
-	const menu = item.create_menu();
-
-	return (
+	const button = (
 		<button
-			className="bar__tray-item"
-			tooltipMarkup={bind(item, "tooltipMarkup")}
-			onDestroy={() => menu?.destroy()}
-			onClickRelease={(self, event) => {
-				menu?.popup_at_widget(
-					self,
-					Gdk.Gravity.SOUTH,
-					Gdk.Gravity.NORTH,
-					null,
-				);
+			className="systray-item bar__tray-item"
+			halign={Gtk.Align.CENTER}
+			valign={Gtk.Align.CENTER}
+			tooltip_markup={bind(item, "tooltip_markup")}
+			focus_on_click={false}
+			use_underline={false}
+			onClick={(btn, event) => {
+				if (event.button === Gdk.BUTTON_PRIMARY) {
+					menu?.popup_at_widget(btn, Gdk.Gravity.EAST, Gdk.Gravity.WEST, null);
+				}
+				if (event.button === Gdk.BUTTON_SECONDARY) {
+					menu?.popup_at_widget(btn, Gdk.Gravity.EAST, Gdk.Gravity.WEST, null);
+				}
+				if (event.button === Gdk.BUTTON_MIDDLE) {
+					item.activate(0, 0);
+				}
 			}}
 		>
-			<icon gIcon={bind(item, "gicon")} />
+			<icon gicon={bind(item, "gicon")} halign={Gtk.Align.CENTER} valign={Gtk.Align.CENTER} />
 		</button>
 	);
+
+	item.connect("notify::menu-model", () => {
+		item.about_to_show()
+		const newMenu = createMenu(item.menu_model, item.action_group);
+		menu.destroy();
+		menu = newMenu;
+	});
+
+	item.connect("notify::action-group", () => {
+		const newMenu = createMenu(item.menu_model, item.action_group);
+		menu.destroy();
+		menu = newMenu;
+	});
+
+	return button;
 };
 
-export default () => {
-	const tray = Tray.get_default();
+const setupTray = (box: Widget.Box) => {
+	const systemTray = AstalTray.Tray.get_default();
+	const items = new Map<string, ReturnType<typeof SysTrayItem>>();
 
-	return (
-		<revealer
-			className={"tray-revealer"}
-			visible={tray.get_items().length > 0}
-			revealChild={tray.get_items().length > 0}
-			transitionDuration={300}
-			transitionType={Gtk.RevealerTransitionType.SLIDE_LEFT}
-			setup={(self) => {
-				self.hook(tray, "notify::items", () => {
-					if (tray.get_items().length > 0) {
-						self.visible = true;
-						self.reveal_child = true;
-					} else {
-						self.reveal_child = false;
-						setTimeout(() => {
-							self.visible = false;
-						}, 300);
-					}
-				});
-			}}
-		>
-			<BarItem className="bar__tray">
-				<box spacing={4} hexpand={false} valign={Gtk.Align.CENTER}>
-					{bind(tray, "items").as((items) =>
-						items.map((item) => {
-							if (item.iconThemePath)
-								App.add_icons(item.iconThemePath);
-							return <BarTrayItem item={item} />;
-						}),
-					)}
-				</box>
-			</BarItem>
-		</revealer>
-	);
+	const addItem = (id: string) => {
+		const item = systemTray.get_item(id);
+		if (item) {
+			const trayItem = SysTrayItem(item);
+			items.set(id, trayItem);
+			box.add(trayItem);
+			trayItem.show();
+		}
+	};
+
+	const removeItem = (id: string) => {
+		const trayItem = items.get(id);
+		if (trayItem) {
+			trayItem.destroy();
+			items.delete(id);
+		}
+	};
+
+	systemTray
+		.get_items()
+		.sort((a, b) => a.item_id.localeCompare(b.item_id))
+		.forEach((item) => addItem(item.item_id));
+	systemTray.connect("item_added", (_, id) => addItem(id));
+	systemTray.connect("item_removed", (_, id) => removeItem(id));
 };
+
+export default () => <box className={"tray container"} halign={Gtk.Align.CENTER} valign={Gtk.Align.CENTER} expand vertical setup={setupTray} />;
