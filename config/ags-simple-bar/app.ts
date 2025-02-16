b05cb1f3d7ebd7c2925e78dc9e7d37e8329bb5e5
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2025-02-16 20:50:06 +0100

    ags implementing gtk3 from scratch, request hyprland toggle

--- config/ags-simple-bar/app.ts
+++ config/ags-simple-bar/app.ts
@@ -5,7 +5,13 @@ import Applauncher from "./widget/app-launcher/Applauncher";
 import OSD from "./widget/osd/OSD";
 import NotificationPopups from "./widget/notifications/NotificationPopups";
 import Dashboard from "./widget/dashboard/Dashboard";
-import { visibleQSMainPage, visiblePowermenu } from "./widget/common/Variables";
+import {
+    visibleQSMainPage,
+    visiblePowermenu,
+    namespaceApplauncher,
+    namespaceNotification,
+    namespaceWeather
+} from "./widget/common/Variables";
 import { reloadScss } from "./lib/utils";
 import WeatherPopup from "./widget/weather/WeatherPopup";
 import NotificationPopupWindow from "./widget/notifications/NotificationPopupWindow";
@@ -42,18 +48,28 @@ function main() {
 
 App.start({
     css: style,
-    instanceName: "js",
+    // instanceName: "js",
     requestHandler(request: string, res: (response: any) => void) {
 		const args = request.split(" ");
 		if (args[0] == "toggle" && args[1]) {
             switch (args[1]) {
+                case "app-launcher":
+                case "launcher":
+                    App.toggle_window(namespaceApplauncher);
+                    break;
                 case "control-center":
                 case "quicksettings":
                     visibleQSMainPage.set(!visibleQSMainPage.get());
                     break;
-            
+                case "notifications":
+                case "notification":
+                    App.toggle_window(namespaceNotification);
+                    break;
                 case "powermenu":
                     visiblePowermenu.set(!visiblePowermenu.get());
+                    break;
+                case "weather":
+                    App.toggle_window(namespaceWeather);
                     break;
                 default:
                     print("Unknown request:", request);
@@ -63,9 +79,6 @@ App.start({
         } else {
             return res("Unknown request");
         }
-
-        print(request);
-        res("ok");
     },
     main: main,
 })
