b852d9091071eab0e1bd431cecdf55a7817e84b3
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2025-02-17 16:07:50 +0100

    ags app.ts file reorganized

--- config/ags/app.ts
+++ config/ags/app.ts
@@ -46,17 +46,22 @@ function main() {
 	}
 
     App.connect("monitor-added", (_, gdkmonitor) => {
+        console.log("monitor added");
 		bars.set(gdkmonitor, Bar(gdkmonitor));
 		notificationsPopups.set(gdkmonitor, NotificationPopups(gdkmonitor));
 		osds.set(gdkmonitor, OSD(gdkmonitor));
 	});
 
 	App.connect("monitor-removed", (_, gdkmonitor) => {
+        console.log("monitor removed");
+
 		bars.get(gdkmonitor)?.destroy();
-		notificationsPopups.get(gdkmonitor)?.destroy();
-		osds.get(gdkmonitor)?.destroy();
 		bars.delete(gdkmonitor);
+
+        notificationsPopups.get(gdkmonitor)?.destroy();
 		notificationsPopups.delete(gdkmonitor);
+
+        osds.get(gdkmonitor)?.destroy();
 		osds.delete(gdkmonitor);
 	});
 }
