63cdffa3abfc49ae99cc6e2f3ab4ce3400076bf7
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2025-09-16 16:30:13 +0200

    ags gtk4 and gtk4 v2 deprecated

--- config/ags-gtk4/widget/ControlCenter/items/ColorScheme.tsx
+++
@@ -1,6 +0,0 @@
-import ControlCenterButton from "../ControlCenterButton";
-import icons from "../../../lib/icons";
-
-export default () => {
-	return <ControlCenterButton icon={icons.colorscheme.dark} />;
-};
